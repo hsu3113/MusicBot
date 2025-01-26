724364c70dc84d1e22c0ce872fd2560c13a541de
hsu3113 <67235160+hsu3113@users.noreply.github.com>
2025-01-26 14:54:52 +0900

    Update music_bot.py

--- music_bot.py
+++ music_bot.py
@@ -11,7 +11,7 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 if not DISCORD_TOKEN:
     raise ValueError("DISCORD_TOKEN이 .env 파일에 정의되어 있지 않습니다.")
-print(DISCORD_TOKEN)
+print(f"DISCORD_TOKEN = {DISCORD_TOKEN} ")
 
 # 봇 초기화
 bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
@@ -19,20 +19,12 @@ bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print(f"connecting...")
-    # 특정 서버 ID
-    GUILD_ID = 629171925976875009  # 테스트할 서버 ID를 입력하세요
-    guild = discord.Object(id=GUILD_ID)
-
-    print(f"connecting.")
-
-    # 특정 서버에서 Slash Command 동기화
-    synced = await bot.tree.sync(guild=guild)
     try:
-        synced = await bot.tree.sync(guild=guild)
-        print(f"Synced {len(synced)} command(s) to guild {GUILD_ID}.")
+        synced = await bot.tree.sync()  # 글로벌 동기화
+        print(f"Synced {len(synced)} global command(s).")
     except Exception as e:
-        print(f"명령어 동기화 중 오류 발생: {e}")
+        print(f"글로벌 명령어 동기화 실패: {e}")
+    print(f"Logged in as {bot.user}")
 
 
 async def main():
